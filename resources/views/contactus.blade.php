ad2cb7093315680dc2e951c4f50173c35edfcff8
Kaboromann <106737102+Kaboromann@users.noreply.github.com>
2023-06-21 14:12:50 +0300

    add contact page

---
+++ resources/views/contactus.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ukhubhuka Contact us</title>
+  <style>
+    
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    header {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+
+    h1 {
+      margin: 0;
+    }
+
+    main {
+      padding: 20px;
+      max-width: 800px;
+      margin: 0 auto;
+    }
+
+    h2 {
+      margin-top: 0;
+    }
+
+    p {
+      margin-bottom: 20px;
+    }
+
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+
+    li {
+      margin-bottom: 10px;
+    }
+
+    footer {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+
+    
+    @media only screen and (max-width: 600px) {
+      header, footer {
+        padding: 10px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+
+      main {
+        padding: 10px;
+      }
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>Contact Us</h1>
+  </header>
+
+  <main>
+    <section>
+      <h2>Get in Touch</h2>
+      <p>We'd love to hear from you! Please use the contact information below to reach us:</p>
+    </section>
+
+    <section>
+      <h2>Contact Information</h2>
+      <ul>
+        <li><strong>Phone:</strong> +1-123-456-7890</li>
+        <li><strong>Email:</strong> info@ukhubhuka.com</li>
+        <li><strong>Address:</strong> Koinange Street, Nairobi, Kenya</li>
+      </ul>
+    </section>
+  </main>
+
+  <footer>
+    <p>&copy; 2023 Ukubhuka. All rights reserved.</p>
+  </footer>
+</body>
+</html>
