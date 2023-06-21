042e10bd20a880a3195321dac8cd7729608a82ce
Kaboromann <106737102+Kaboromann@users.noreply.github.com>
2023-06-21 15:04:43 +0300

    add changes

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -3,28 +3,72 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Thank You - Your Tourist Website</title>
+  <title>Thank You - ukhubhuka</title>
   <style>
-    
+    /* CSS Styles */
     body {
       font-family: Arial, sans-serif;
-      text-align: center;
-      background-color:  #ffc107
-      padding: 50px;
+      background-color:  #fff;
+      margin: 0;
+      padding: 0;
     }
-    
+
+    .container {
+      max-width: 500px;
+      margin: 0 auto;
+      padding: 50px;
+      background-color:  #ffc107;
+      border-radius: 5px;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+
     h1 {
+      color: #333;
       margin-top: 0;
     }
 
     p {
+      color: #555;
       margin-bottom: 20px;
+    }
+
+    .thank-you-message {
+      font-size: 18px;
+      line-height: 1.4;
+    }
+
+    .thank-you-message strong {
+      color: #333;
+    }
+
+    .back-to-home {
+      display: block;
+      text-align: center;
+      margin-top: 30px;
+    }
+
+    .back-to-home a {
+      color: #333;
+      text-decoration: none;
+      font-weight: bold;
+    }
+
+    /* Media Queries */
+    @media only screen and (max-width: 600px) {
+      .container {
+        padding: 30px;
+      }
     }
   </style>
 </head>
 <body>
-  <h1>Thank You for Your Review!</h1>
-  <p>We appreciate your feedback. It helps us improve our services and provide a better experience for our customers.</p>
-  <p>Thank you again for taking the time to share your thoughts with us!</p>
+  <div class="container">
+    <h1>Thank You for Your Review!</h1>
+    <div class="thank-you-message">
+      <p>We appreciate your feedback. It helps us improve our services and provide a better experience for our customers.</p>
+      <p>Thank you again for taking the time to share your thoughts with us!</p>
+    </div>
+    
+  </div>
 </body>
 </html>
